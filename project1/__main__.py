ca462c1c8a0de5bfeea687eedeb32bc5c37ce748
Weronika Piotrowska <piotrowska.w00@gmail.com>
2024-03-17 12:30:40 +0100

    added padding  + fixed warnings

--- project1/__main__.py
+++ project1/__main__.py
@@ -1,18 +1,25 @@
+# supress PNG warning: iCCP: known incorrect sRGB profile
+# must be set before import tensorflow
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 from test_case import run_test_case
+from dataset_preparation import *
+import logging
 import tensorflow as tf
+
+
 
 layers = [1, 3, 5]
 kernel_sizes = [3, 5]
-padding = [0, 1, 2]  # TODO
+padding = [0, 1, 2] 
 stride = [1, 2, 3, 5]  # TODO
 pooling = [None, 'max_pooling', 'average_pooling']
 normalization = [None, 'batch', 'layer']  # TODO
 optimizeers = [None, 'adam', 'SGD', 'rmsprop', 'adagrad']
 
-# PNG warning: iCCP: known incorrect sRGB profile
-tf.autograph.set_verbosity(
-    level=0, alsologtostdout=False
-)
-
-
-run_test_case(5, 3, 0, 1, 'average_pooling', 'none', 'rmsprop')
+train_ds = get_train_data(128)
+val_ds = get_validation_data()
+#test_ds = get_test_data()
+run_test_case(layers=3, kernel_size=3, padding=0, stride=1, pooling='max_pooling',
+              normalization=None, optimizer='adam', train_ds=train_ds, val_ds=val_ds, test_ds=None)
--- project1/test_case.py
+++ project1/test_case.py
@@ -1,14 +1,13 @@
 
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
-from dataset_preparation import *
 import warnings
 
 
 N_EPOCHS = 100
 
 
-def run_test_case(layers, kernel_size, padding, stride, pooling, normalization, optimizer):
+def run_test_case(layers, kernel_size, padding, stride, pooling, normalization, optimizer, train_ds, val_ds, test_ds):
 
     match(layers):
         case 1:
@@ -22,69 +21,113 @@ def run_test_case(layers, kernel_size, padding, stride, pooling, normalization, 
             model = get_5_layer_model(
                 kernel_size, padding, stride, pooling, normalization, optimizer)
 
+    model.build()
     print(model.summary())
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy')
-    model.fit(get_train_data(128), epochs=N_EPOCHS,
-              validation_data=get_validation_data())
+    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=["accuracy"])
+    model.fit(train_ds, epochs=N_EPOCHS,
+              validation_data=val_ds)
 
 
 def get_1_layer_model(kernel_size, padding, stride, pooling, normalization, optimizer):
     model = models.Sequential()
+
+    # CONVOLUTION LAYER 1
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(
+            padding=(padding, padding), input_shape=(32, 32, 3)))
     model.add(layers.Conv2D(32, (kernel_size, kernel_size),
-                            activation='relu', input_shape=(32, 32, 3)))
+                            activation='relu', input_shape=(32 + padding, 32 + padding, 3)))
     model = add_pooling_layer(model, pooling)
 
+    # DENSE LAYERS
     model.add(layers.Flatten())
     model.add(layers.Dense(32, activation='relu'))
-    model.add(layers.Dense(10))
+    model.add(layers.Dense(10, activation='softmax'))
 
     return model
 
 
 def get_3_layer_model(kernel_size, padding, stride, pooling, normalization, optimizer):
     model = models.Sequential()
+
+    # CONVOLUTION LAYER 1
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(
+            padding=(padding, padding), input_shape=(32, 32, 3)))
     model.add(layers.Conv2D(32, (kernel_size, kernel_size),
-                            activation='relu', input_shape=(32, 32, 3)))
+                            activation='relu', input_shape=(32 + padding, 32 + padding, 3)))
     model = add_pooling_layer(model, pooling)
 
-    model.add(layers.Conv2D(64, (kernel_size, kernel_size), activation='relu'))
+    # CONVOLUTION LAYER 2
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(padding=(padding, padding)))
+    model.add(layers.Conv2D(64, (kernel_size, kernel_size),
+              activation='relu'))
     model = add_pooling_layer(model, pooling)
 
-    model.add(layers.Conv2D(64, (kernel_size, kernel_size), activation='relu'))
+    # CONVOLUTION LAYER 3
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(padding=(padding, padding)))
+    model.add(layers.Conv2D(64, (kernel_size, kernel_size),
+              activation='relu'))
     model = add_pooling_layer(model, pooling)
 
+    # DENSE LAYERS
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10))
+    model.add(layers.Dense(10,  activation='softmax'))
     return model
 
 
 def get_5_layer_model(kernel_size, padding, stride, pooling, normalization, optimizer):
-
     model = models.Sequential()
+
+    # CONVOLUTION LAYER 1
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(
+            padding=(padding, padding), input_shape=(32, 32, 3)))
     model.add(layers.Conv2D(32, (kernel_size, kernel_size),
-                            activation='relu', input_shape=(32, 32, 3)))
+                            activation='relu', input_shape=(32 + padding, 32 + padding, 3)))
+
+    # CONVOLUTION LAYER 2
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(padding=(padding, padding)))
+    model.add(layers.Conv2D(32, (kernel_size, kernel_size), activation='relu'))
     model = add_pooling_layer(model, pooling)
 
+    # CONVOLUTION LAYER 3
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(padding=(padding, padding)))
+    model.add(layers.Conv2D(64, (kernel_size, kernel_size), activation='relu'))
+
+    # CONVOLUTION LAYER 4
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(padding=(padding, padding)))
     model.add(layers.Conv2D(64, (kernel_size, kernel_size), activation='relu'))
     model = add_pooling_layer(model, pooling)
 
+    # CONVOLUTION LAYER 5
+    if padding > 0:
+        model.add(layers.ZeroPadding2D(padding=(padding, padding)))
     model.add(layers.Conv2D(64, (kernel_size, kernel_size), activation='relu'))
-    add_pooling_layer(model, pooling)
 
-    model.add(layers.Conv2D(64, (kernel_size, kernel_size), activation='relu'))
-    add_pooling_layer(model, pooling)
-
+    # DENSE LAYERS
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(10))
+    model.add(layers.Dense(10, activation='softmax'))
     return model
 
 
-def add_pooling_layer(model, pooling):
+def add_pooling_layer(model, pooling, pooling_size=2):
     match(pooling):
         case 'max_pooling':
-            model.add(layers.MaxPooling2D((2, 2)))
+            model.add(layers.MaxPooling2D((pooling_size, pooling_size)))
         case 'average_pooling':
-            model.add(layers.AveragePooling2D((2, 2)))
+            model.add(layers.AveragePooling2D((pooling_size, pooling_size)))
+        case _:
+            pass
     return model
+
+
+def get_next_dimension(input_vol, kernel, padding, stride):
+    return (input_vol - kernel + 2*padding)/stride + 1
