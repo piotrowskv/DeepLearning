531bd57c35fba77aa269c881f70b891ec6f83b2f
Weronika Piotrowska <piotrowska.w00@gmail.com>
2024-06-10 21:15:08 +0200

    temporary fid

--- project3/__main__.py
+++ project3/__main__.py
@@ -8,7 +8,7 @@ from load_data import preprocess_data, load_data
 from diffusion_model import *
 from train import train
 from generate import *
-
+import os
 
 # Load the dataset
 train_dir = 'all_images'
@@ -17,26 +17,38 @@ image_size = (64, 64)
 T = [2, 50, 100]
 diffs = [0.5]
 seeds = [42, 51, 128, 345, 11]
-model_numbers = [1, 2, 3]
+model_numbers = [3]
 epochs = 3
-train_dataset = load_data(
-    train_dir, image_size=image_size, batch_size=batch_size)
+# train_dataset = load_data(
+#     train_dir, image_size=image_size, batch_size=batch_size)
 
-train_dataset = preprocess_data(train_dataset)
-for seed in seeds:
-    tf.random.set_seed(seed)
-    np.random.seed(seed)
+# train_dataset = preprocess_data(train_dataset)
+# for seed in seeds:
+#     tf.random.set_seed(seed)
+#     np.random.seed(seed)
 
-    for t in T:
-        for diff in diffs:
-            for model_num in model_numbers:
-                model_name = "model-n" + \
-                    str(model_num) + "-seed" + str(seed) + \
-                    "-timesteps" + str(t) + "diff-" + str(diff)
-                model = train(model_number=model_num, train_dataset=train_dataset, epochs=epochs,
-                              diffusion_coefficient=diff, T=t, model_name=model_name)
-                # generated_images =  generate_images(model, 64, model_name + 'img', t)
-                # fid = calculate_fid(
-                #     train_dataset.take(64), generated_images)
-                # with open(model_name + ".txt", "w+") as f:
-                #     f.write(fid)
+    # for t in T:
+    #     for diff in diffs:
+    #         for model_num in model_numbers:
+    #             model_name = "2model-n" + \
+    #                 str(model_num) + "-seed" + str(seed) + \
+    #                 "-timesteps" + str(t) + "diff-" + str(diff)
+    #             model = train(model_number=model_num, train_dataset=train_dataset, epochs=epochs,
+    #                           diffusion_coefficient=diff, T=t, model_name=model_name)
+#model = keras.saving.load_model("2model-n1-seed42-timesteps2diff-0.5.keras")
+real_images = []
+for root, _, files in os.walk('less_images'):
+    for file in files:
+        if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):  # Add more extensions if needed
+            real_images.append(os.path.join(root, file))
+
+generated_images = [
+    r"model-n2-seed42-timesteps2diff-0.61.png",
+    r"model-n2-seed42-timesteps2diff-0.62.png",
+    r"model-n2-seed42-timesteps2diff-0.63.png",
+
+]
+fid = calculate_fid(real_images, generated_images)
+print(fid)
+# with open(model_name + ".txt", "w+") as f:
+#     f.write(fid)
--- project3/generate.py
+++ project3/generate.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from diffusion_model import *
 from scipy.linalg import sqrtm
 from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.models import Model
 
 
 
@@ -23,16 +24,26 @@ def generate_images(model, num_images, save_path, T):
         plt.savefig(save_path + '.png')
         return generated_images
 
-def resize(img):
-    return tf.image.resize(img, (299, 299))
+def resize(image_list):
+    new_image_list = []
+    for img in image_list:
+        new_img = tf.keras.utils.load_img(img, target_size=(64, 64, 3))
+        new_image_list.append(tf.image.resize(new_img, (75, 75)))
+    return new_image_list
+
 def calculate_fid(real_images, generated_images):
     inception_model = InceptionV3(
-        include_top=False, pooling='avg', input_shape=(299, 299, 3))
-    real_images = tf.map_fn(resize, real_images)
-    generated_images = tf.map_fn(resize, generated_images)
+        include_top=False, pooling='avg', input_shape=(75, 75, 3))
+    
+    # real_images = tf.map_fn(resize, real_images)
+    # generated_images = tf.map_fn(resize, generated_images)
+    real_images = resize(real_images)
+    generated_images = resize(generated_images)
     # Extract features
-    real_features = inception_model.predict(real_images)
-    generated_features = inception_model.predict(generated_images)
+    real_features = inception_model.predict(
+        tf.data.Dataset.from_tensor_slices(real_images).batch(1))
+    generated_features = inception_model.predict(
+        tf.data.Dataset.from_tensor_slices(generated_images).batch(1))
 
     # Calculate mean and covariance
     mu_real, sigma_real = real_features.mean(
@@ -47,3 +58,66 @@ def calculate_fid(real_images, generated_images):
         covmean = covmean.real
     fid = ssdiff + np.trace(sigma_real + sigma_generated - 2.0 * covmean)
     return fid
+
+
+# def calculate_fid(image1, image2):
+#     def load_image(image_path):
+#         img = tf.io.read_file(image_path)
+#         img = tf.image.decode_image(img, channels=3)
+#         # Resize to InceptionV3 input size
+#         img = tf.image.resize(img, (299, 299))
+#         img = tf.keras.applications.inception_v3.preprocess_input(img)
+#         return img
+
+#     def extract_features(images):
+#         # Load the InceptionV3 model pre-trained on ImageNet
+#         base_model = InceptionV3(
+#             include_top=False, pooling='avg', input_shape=(75, 75, 3))
+#         model = Model(inputs=base_model.input, outputs=base_model.output)
+
+#         # Extract features
+#         features = model.predict(images)
+#         return features
+
+#     def calculate_statistics(features):
+#         mu = np.mean(features, axis=0)
+#         sigma = np.cov(features, rowvar=False)
+#         return mu, sigma
+
+#     def calculate_fid2(mu1, sigma1, mu2, sigma2):
+#         # Calculate the sum of the covariance matrices
+#         print(sigma1)
+#         print(sigma2)
+#         # sigma1 += np.eye(sigma1.shape[0]) * 1e-6
+#         # sigma2 += np.eye(sigma2.shape[0]) * 1e-6
+#         # covmean = sqrtm(sigma1.dot(sigma2))
+#         covmean = np.sqrt(sigma1 * sigma2)
+#         # Numerical stability
+#         if np.iscomplexobj(covmean):
+#             covmean = covmean.real
+
+#         # Calculate the Fréchet distance
+#         # fid = np.sum((mu1 - mu2) ** 2) + \
+#         #     np.trace(sigma1 + sigma2 - 2 * covmean)
+#         fid = np.sum((mu1 - mu2) ** 2) + \
+#            (sigma1 + sigma2 - 2 * covmean)
+#         print(fid)
+#         return fid
+
+
+#     # Create a batch of images
+#     image1 = tf.image.resize(image1, (75, 75))
+#     image2 = tf.image.resize(image2, (75, 75))
+
+#     images = tf.stack([image1, image2])
+
+#     # Extract features
+#     features = extract_features(images)
+
+#     # Compute statistics
+#     mu1, sigma1 = calculate_statistics(features[:1])
+#     mu2, sigma2 = calculate_statistics(features[1:])
+
+#     # Compute FID
+#     fid = calculate_fid2(mu1, sigma1, mu2, sigma2)
+#     return fid
